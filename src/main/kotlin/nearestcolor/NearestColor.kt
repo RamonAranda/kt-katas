8e5798c39fc71221ca18815d6a4d7e4cd7775c45
Ricard Osorio <celtric@gmail.com>
2022-10-28 12:35:37 +0200

    Find multiple colors at the same time

--- src/main/kotlin/nearestcolor/NearestColor.kt
+++ src/main/kotlin/nearestcolor/NearestColor.kt
@@ -1,10 +1,16 @@
 package nearestcolor
 
-enum class Color { Red, Green, Blue}
+enum class Color { Red, Green, Blue }
 
-fun nearestColors(hexadecimalValue: String): List<Color> = when (hexadecimalValue) {
-    "FF0000" -> listOf(Color.Red)
-    "00FF00" -> listOf(Color.Green)
-    "0000FF" -> listOf(Color.Blue)
-    else -> throw Exception("Unknown color")
-}
+fun nearestColors(hexadecimalValue: String): List<Color> = hexadecimalValue
+    .chunked(2)
+    .mapIndexed { index, hexadecimalPortion ->
+        when (index) {
+            0 -> Pair(Color.Red, hexadecimalPortion)
+            1 -> Pair(Color.Green, hexadecimalPortion)
+            2 -> Pair(Color.Blue, hexadecimalPortion)
+            else -> throw Exception("Invalid hexadecimal value")
+        }
+    }
+    .filter { it.second == "FF" }
+    .map { it.first }
--- src/test/kotlin/nearestcolor/NearestColorsTests.kt
+++ src/test/kotlin/nearestcolor/NearestColorsTests.kt
@@ -10,4 +10,11 @@ class NearestColorsTests {
         assertEquals(listOf(Color.Green), nearestColors("00FF00"))
         assertEquals(listOf(Color.Blue), nearestColors("0000FF"))
     }
+
+    @Test fun `multiple colors can be an exact match`() {
+        assertEquals(listOf(Color.Red, Color.Green), nearestColors("FFFF00"))
+        assertEquals(listOf(Color.Red, Color.Blue), nearestColors("FF00FF"))
+        assertEquals(listOf(Color.Green, Color.Blue), nearestColors("00FFFF"))
+        assertEquals(listOf(Color.Red, Color.Green, Color.Blue), nearestColors("FFFFFF"))
+    }
 }
