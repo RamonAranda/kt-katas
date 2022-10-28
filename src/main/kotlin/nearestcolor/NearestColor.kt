1882c80345e3fe71193cad929ef13ad1e54a44a8
Ricard Osorio <celtric@gmail.com>
2022-10-28 12:23:47 +0200

    Go green

--- src/main/kotlin/nearestcolor/NearestColor.kt
+++ src/main/kotlin/nearestcolor/NearestColor.kt
@@ -2,4 +2,7 @@ package nearestcolor
 
 enum class Color { Red, Green, Blue}
 
-fun nearestColors(hexadecimalValue: String): List<Color> = TODO()
+fun nearestColors(hexadecimalValue: String): List<Color> = when (hexadecimalValue) {
+    "FF0000" -> listOf(Color.Red)
+    else -> throw Exception("Unknown color")
+}
